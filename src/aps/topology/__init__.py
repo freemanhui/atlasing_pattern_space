b26a2b28e48460e1aa2989967449793d5470717d
3man <sempe@hotmail.com>
2025-10-24 12:18:51 +0800

    fix: correct topology module import and add plotly to dev deps

--- src/aps/topology/__init__.py
+++ src/aps/topology/__init__.py
@@ -1,4 +1,4 @@
 from .topo_autoencoder import TopologicalAutoencoder, TopoAEConfig
-from .topo_loss import KNNTopoLoss, knn_indices, adjacency_from_knn
+from .knn_topo_loss import KNNTopoLoss, knn_indices, adjacency_from_knn
 
 __all__ = ['TopologicalAutoencoder', 'TopoAEConfig', 'KNNTopoLoss', 'knn_indices', 'adjacency_from_knn']
