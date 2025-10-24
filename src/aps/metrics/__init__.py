04746598b402d18269fcd8fa0833449650e565cd
3man <sempe@hotmail.com>
2025-10-24 12:25:51 +0800

    fix: correct metrics module import (basin -> energy)

--- src/aps/metrics/__init__.py
+++ src/aps/metrics/__init__.py
@@ -1,5 +1,5 @@
 from .topo import knn_preservation, trustworthiness
-from .basin import basin_depth
+from .energy import basin_depth
 from .pointer_strength import pointer_strength
 
 __all__ = ['knn_preservation', 'trustworthiness', 'basin_depth', 'pointer_strength']
